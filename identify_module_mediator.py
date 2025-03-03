f7bcd2129abbcb9af680292c0b72d21ff8737204
shazhendong <zhendong.alan.sha@gmail.com>
2025-03-03 13:58:45 -0500

    Update identify_module_mediator.py

--- identify_module_mediator.py
+++ identify_module_mediator.py
@@ -102,20 +102,21 @@ if __name__ == "__main__":
     # 5. add mediator genes to the network with edges to the linked pairs
     mappingGene = mg.init_geneName_mapping()
     # print(arr_mediator_gene)
+    scr_query = network_gml.split('.')[0] + "_mediator"
     for i in range(len(arr_mediator_gene)):
         mediator_gene = arr_mediator_gene[i]
         pairsLinked = arr_pairsLinked[i]
         # add mediator gene to the network, with attributes symbol and query equal to "network_gml".split('.')[0] + "_mediator"
         # print(f"Mediator gene: {mediator_gene}")
-        G_module.add_node(mediator_gene + "_mediator", symbol=mg.map_NCBI_to_HGNC_to_symbol(mediator_gene, mappingGene), query=network_gml.split('.')[0] + "_mediator")
+        G_module.add_node(mediator_gene + "_" + scr_query, symbol=mg.map_NCBI_to_HGNC_to_symbol(mediator_gene, mappingGene), query=network_gml.split('.')[0])
         # add edges between mediator gene and linked pairs with attribute query equal to "network_gml".split('.')[0] + "_mediator"
         for pair in pairsLinked:
             # check if mediator_gene, pair[0] is not in G_module
-            if not G_module.has_edge(mediator_gene + "_mediator", pair[0]):
-                G_module.add_edge(mediator_gene + "_mediator", pair[0], query=network_gml.split('.')[0] + "_mediator")
+            if not G_module.has_edge(mediator_gene + "_" + scr_query, pair[0]):
+                G_module.add_edge(mediator_gene + "_" + scr_query, pair[0], query=network_gml.split('.')[0])
             # check if mediator_gene, pair[1] is not in G_module
-            if not G_module.has_edge(mediator_gene + "_mediator", pair[1]):
-                G_module.add_edge(mediator_gene + "_mediator", pair[1], query=network_gml.split('.')[0] + "_mediator")
+            if not G_module.has_edge(mediator_gene + "_" + scr_query, pair[1]):
+                G_module.add_edge(mediator_gene + "_" + scr_query, pair[1], query=network_gml.split('.')[0])
     
     # 6. save the network with mediator genes
     output_gml = "mediated_" + network_gml
